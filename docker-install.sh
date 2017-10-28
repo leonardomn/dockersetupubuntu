e879317da2fc9a99355624fed7eef75bb684aba1
Leonardo Miranda <leonardo@srvdesk.com.br>
2017-10-27 23:54:27 -0200

    Script Updated

--- docker-install.sh
+++ docker-install.sh
@@ -4,9 +4,11 @@ echo "Installing Docker-CE (Community Edition) for Ubuntu 17.04 AMD64 >"
 echo .......
 
 echo "Update the apt packeage index:"
+echo .......
 sudo apt update
 
 echo "Install packages to allow apt to use a repository over HTTPS:"
+echo .......
 sudo apt install -y \
     apt-transport-https \
     ca-certificates \
@@ -14,19 +16,27 @@ sudo apt install -y \
     software-properties-common
 
 echo "Dockers official GPG key:"
+echo ......
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 echo "Verify if we have the key with the fingerprint"
+echo ......
 sudo apt-key fingerprint 0EBFCD88
 
 echo "Adding repository for install:"
+echo ......
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
 echo "Updating packages for Docker-CE:"
+echo ......
 sudo apt update
 
-echo "Installing Docker-CE (Community Edition)"
+echo "Installing Docker-CE (Community Edition)...."
+echo ......
 sudo apt install docker-ce -y
+
+echo "Testing Docker..."
+sudo docker run hello-world
